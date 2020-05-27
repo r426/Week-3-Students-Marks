1126c1b666992bb0aed079f1f620b2a54e3d2821
Your Name <rutaslv@hotmail.lt>
2020-05-27 20:47:35 +0300

    Checks if userInput.isnumeric()

--- studentsMarks.py
+++ studentsMarks.py
@@ -8,23 +8,23 @@ sum = 0
 for i in range(numberOfMarks):
     enteredMark = -1
     while enteredMark < 0 or enteredMark > 100:
-        enteredMark = int(input('\nEnter the mark:'))
+        userInput = input('\nEnter the mark:')
+        if userInput.isnumeric():
+            enteredMark = int(userInput)
+            if enteredMark > 100:
+                print("Input error.")
+        else: print("Input error.")
     marks.append(enteredMark)
     sum += marks[i]
     for j in range(len(marks)):
         print('Mark No.{} is {}'.format(j + 1, marks[j]))
 
 average = sum / numberOfMarks
-if average >= 90:
-    grade = 'A'
-elif 90 > average >= 80:
-    grade = 'B'
-elif 80 > average >= 70:
-    grade = 'C'
-elif 70 > average >= 60:
-    grade = 'D'
-else:
-    grade = 'F'
+if average >= 90: grade = 'A'
+elif average >= 80: grade = 'B'
+elif average >= 70: grade = 'C'
+elif average >= 60: grade = 'D'
+else: grade = 'F'
 
 print('\nYour marks:', end=' ')
 for x in marks:
